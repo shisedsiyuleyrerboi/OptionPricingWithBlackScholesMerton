33ddbb79c128249f0633c7f066cea26029fb38df
Francisco Luna Ratto <franciscolunaratto@icloud.com>
2025-11-28 00:27:56 -0500

    Update part1.py

--- src/parts/part1.py
+++ src/parts/part1.py
@@ -6,5 +6,4 @@ prob_1 = stats.norm.cdf(values_p1[0], loc=mean, scale=sd)
 prob_2 = 1 - stats.norm.cdf(values_p1[1], loc=mean, scale=sd)
 
 print(f'P(R < 0.0)   = {prob_1:.4f}')
-
 print(f'P(R > 0.2)   = {prob_2:.4f}')
